76689ba2eb3cd691523fc7325e1d466b4b2e31bc
randocal <rdsoundguy@gmail.com>
2023-03-12 14:25:13 -0400

    Sidebar setup but ont working....??

--- react-dash/src/scenes/global/Sidebar.jsx
+++ react-dash/src/scenes/global/Sidebar.jsx
@@ -1,7 +1,63 @@
+import { useState } from "react";
+import { ProSideBar, Menu, MenuItem } from "react-pro-sidebar";
+
+// had to install 'npm i react-pro-sidebar@0.7.1'
+import 'react-pro-sidebar/dist/css/styles.css';
+
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { Link } from "react-router-dom";
+import { tokens } from "../../theme";
+
+import HomeOutlinedIcon from '@mui/icons-material/HomeOutlined';
+import PeopleOutlinedIcon from '@mui/icons-material/PeopleOutlined';
+import ContactsOutlinedIcon from '@mui/icons-material/ContactsOutlined';
+import ReceiptOutlinedIcon from '@mui/icons-material/ReceiptOutlined';
+import PersonOutlinedIcon from '@mui/icons-material/PersonOutlined';
+import CalendarTodayOutlinedIcon from '@mui/icons-material/CalendarTodayOutlined';
+import HelpOutlinedIcon from '@mui/icons-material/HelpOutlined';
+import BarChartOutlinedIcon from '@mui/icons-material/BarChartOutlined';
+import PieChartOutlineOutlinedIcon from '@mui/icons-material/PieChartOutlineOutlined';
+import TimelineOutlinedIcon from '@mui/icons-material/TimelineOutlined';
+import MenuOutlinedIcon from '@mui/icons-material/MenuOutlined';
+import MapOutlinedIcon from '@mui/icons-material/MapOutlined';
+
+
+
+
 
 const Sidebar = () => {
-    return <div> Sidebar</div>  
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+    const [isCollapsed, setIsCollapsed] = useState(false);
 
+    // state for which icon is selected
+    const [selected, setSelected] = useState("Dashboard");
+
+    return(
+        <Box>
+            TEST
+            {/* overwriting some of the default themes values */}
+            sx={{
+                "& .pro-sidebar-inner": {
+                    background: `${colors.primary [400]} !important`
+                },
+                "& .pro-icon-wrapper": {
+                    backgroundColor: "transparent !important"
+                },
+                "& .pro-inner-item": {
+                    padding: "5px 35px 5px 20px !important"
+                },
+                "& .pro-inner-item:hover": {
+                    color: "#868dfb !important"
+                },
+                "& .pro-menu-item.active": {
+                    color: "#6870fa !important"
+
+                }
+            }}
+
+        </Box>
+    )
 }
 
 
